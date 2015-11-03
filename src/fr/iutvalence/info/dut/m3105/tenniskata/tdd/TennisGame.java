9687189140a9eedd0279ec24f3dc5844aa9d5459
vanbossm <maxime.vanbossel@iut-valence.fr>
2015-11-03 17:10:16 +0100

    test JUnit

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -1,5 +1,63 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
+	private String score;
+	private int scoreServer;
+	private int scoreReceiver;
+	
+	public TennisGame(String score, int scoreServer, int scoreReceiver){
+		this.score = "love_all";
+		this.scoreServer = scoreServer;
+		this.scoreReceiver = scoreReceiver;
+	}
+	
+	public void setScore(String score)
+	{
+		this.score = score;
+	}
 
+	public String getScore()
+	{
+		if (this.scoreServer == 1 & this.scoreReceiver == 0)
+		{
+			this.score = "fifteen_love";
+		}
+		
+		if (this.scoreServer == 0 & this.scoreReceiver == 1)
+		{
+			this.score = "love_fifteen";
+		}
+		
+		if (this.scoreServer == 1 & this.scoreReceiver == 1)
+		{
+			this.score = "fifteen_all";
+		}
+		
+		return score;
+	}
+
+	
+	
+	public int getScoreServer()
+	{
+		return scoreServer;
+	}
+
+	public void setScoreServer(int scoreServer)
+	{
+		this.scoreServer = scoreServer;
+	}
+
+	public int getScoreReceiver()
+	{
+		return scoreReceiver;
+	}
+
+	public void setScoreReceiver(int scoreReceiver)
+	{
+		this.scoreReceiver = scoreReceiver;
+	}
+	
+	
 }
+
--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -1,15 +1,38 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class TennisGameTest
 {
 	@Test
-	public void aSimpleBeginningTest()
+	public void loveAll()
 	{
-		Assert.fail("...");
+		TennisGame score = new TennisGame("love_all",0,0);
+		assertEquals(score.getScore(), "love_all");
+	}
+	
+	@Test
+	public void ifServerHasScoredOnePointScoreIsFifteenLove()
+	{
+		TennisGame score = new TennisGame("fifteen_love",1,0);
+		assertEquals(score.getScore(), "fifteen_love");
+	}
+	
+	@Test
+	public void ifReceiverHasScoredOnePointScoreIsLoveFifteen()
+	{
+		TennisGame score = new TennisGame("love_fifteen",0,1);
+		assertEquals(score.getScore(), "love_fifteen");	
+	}
+	
+	@Test
+	public void ifReceiverAndServerHaveScoredOnePointScoreIsFifteenAll()
+	{
+		TennisGame score = new TennisGame("fifteen_all",1,1);
+		assertEquals(score.getScore(), "fifteen_all");
 	}
 
 }
